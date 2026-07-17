032f9666a144a6dfe04378aa16de1c7c97994174
kitus1000 <jesus12398@gmail.com>
2026-07-17 15:31:58 -0600

    Corregir importacion de useEffect en page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/utils/supabase/client'
 import Link from 'next/link'
